06f3dab4515b133a04b91979f57a2432f369a824
mayyaannkk <mayankpant.dev@gmail.com>
2026-03-01 12:28:26 +0530

    mockito tests for vetService

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService vetService;
+
+    @Test
+    void deleteById() {
+        vetService.deleteById(1L);
+        verify(vetRepository).deleteById(1L);
+    }
+}
