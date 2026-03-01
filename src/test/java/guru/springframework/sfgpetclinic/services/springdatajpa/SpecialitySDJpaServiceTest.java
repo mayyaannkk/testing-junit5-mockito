e0c5c88a77024fee467c1d9e749679c469470c16
mayyaannkk <mayankpant.dev@gmail.com>
2026-03-01 11:13:06 +0530

    verify interaction with mock

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
@@ -27,5 +28,26 @@ class SpecialitySDJpaServiceTest {
     @Test
     void deleteById() {
         specialitySDJpaService.deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdVerify() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+
+        // Here it is checking if the repository was called with this argument value, 1L here, and how many times it was called with that value
+        verify(specialtyRepository, times(2)).deleteById(1L);
+
+        // repository called at least 2 times with argument 1L
+        verify(specialtyRepository, atLeast(2)).deleteById(1L);
+
+        // repository called at most 2 times with argument 1L
+        verify(specialtyRepository, atMost(2)).deleteById(1L);
+
+        // check if repository called with value 1L at least once
+        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+
+        // check if repository never called with value 5L
+        verify(specialtyRepository, never()).deleteById(5L);
     }
 }
